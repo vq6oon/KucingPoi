990d054c7e8d0af86130b8165f7b8cc05d508a20
vq6oon <saviopio03@gmail.com>
2025-10-18 11:02:16 +0700

    Revert "Implement KernelSU"
    This reverts commit 27a0a9e59c702190d5f481eba1e435dee311407b.

--- fs/exec.c
+++ fs/exec.c
@@ -1723,12 +1723,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-// KernelSU hook
-#ifdef CONFIG_KSU
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
-#endif
-
 /*
  * sys_execve() executes a new program.
  */
@@ -1742,10 +1736,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-
-#ifdef CONFIG_KSU
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
